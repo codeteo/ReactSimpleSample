fbffe0f18a0b9e10eee958c276db6d008215a2f6
teo <ramones.teo@gmail.com>
2018-06-22 14:36:02 +0300

    Gets data using Axios

--- client_src/src/components/Meetups.js
+++ client_src/src/components/Meetups.js
@@ -1,6 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class Meetups extends Component {
+    constructor(){
+        super();
+        this.state = {
+            meetups: []
+        }
+    }
+
+    componentWillMount(){
+        this.getMeetups();
+    }
+
+    getMeetups(){
+        axios.get('http://localhost:3000/api/meetups')
+            .then(response => {
+                this.setState({meetups: response.data}, () => {
+                    // console.log(this.state);
+                })
+            })
+    }
+
     render(){
         return (
             <div>
