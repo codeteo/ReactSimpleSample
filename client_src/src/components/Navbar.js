e124640baa65dcb1ae0a61b051f85ad64ab1d8b7
teo <ramones.teo@gmail.com>
2018-06-22 14:26:22 +0300

    Adds Axios dependency

--- client_src/src/components/Navbar.js
+++ client_src/src/components/Navbar.js
@@ -6,17 +6,18 @@ class Navbar extends Component {
         return (
             <div>
                 <nav className="blue darken-3">
-                    <div className="nav-wrapper">
-                        <a href="/" className="brand-logo">Meetupz</a>
-                        <a data-activates="main-menu" className="button-collapse show-on-large"><i className="fa fa-bars"></i></a>
-                        <ul className="right hide-on-small-only">
-                            <li><Link to="/"><i className="fa fa-users"></i>Meetupz</Link></li>
-                        </ul>
-                        <ul className="side-nav" id="main-menu">
-                            <li><Link to="/"><i className="fa fa-users"></i>Meetupz</Link></li>
-                             <li><Link to="/meetups/add"><i className="fa fa-plus"></i>Add Meetup</Link></li>
-                        </ul>
-                    </div>
+                <div className="nav-wrapper">
+                    <a href="/" className="brand-logo center">Meetupz</a>
+                    <a data-activates="main-menu" className="button-collapse show-on-large"><i className="fa fa-bars"></i></a>
+                    <ul className="right hide-on-small-only">
+                     <li><Link to="/"><i className="fa fa-users"></i> Meetups</Link></li>         
+                    </ul>
+                    <ul className="side-nav" id="main-menu">
+                     <li><Link to="/"><i className="fa fa-users"></i> Meetups</Link></li>  
+                     <li><Link to="/meetups/add"><i className="fa fa-plus"></i> Add Meetup</Link></li>  
+                     <li><Link to="/about"><i className="fa fa-question-circle"></i> About</Link></li> 
+                    </ul>
+                </div>
                 </nav>
             </div>
         )
